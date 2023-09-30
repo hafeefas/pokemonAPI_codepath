313a106dc05fec73ba4caf02e5a91ae3decd1182
Hafeefa <hafeefa40@gmail.com>
2023-09-29 23:25:46 -0400

    fixing folder conflicts

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+const PORT = "8080"
+const axios = require("axios")
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+
+const serverRun = () => {
+    app.listen(PORT, () => {
+        console.log(`live on port: ${PORT}`)
+    })
+}
+
+app.get("/getPokemon", async (req, res) => {
+    try{
+        const result = await axios.get("https://pokeapi.co/api/v2/pokemon/")
+        const data = result.data
+        res.send(data)
+    } catch (error){
+        console.log(error)
+        res.status(500).json({ error:"Internal Server Error" })
+    }
+})
+
+serverRun()
+
+module.exports = serverRun
